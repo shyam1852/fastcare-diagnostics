a75a0b380df392f7b1fe022a991c614d430b2a87
Shyam Keshri <shyamkeshri1852@gmail.com>
2026-07-29 21:36:52 +0530

    Use Gmail service

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    service: "gmail",
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
