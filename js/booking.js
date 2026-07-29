f87afea340618968040a6a30de53f7b7e7c41aa9
Shyam Keshri <shyamkeshri1852@gmail.com>
2026-07-29 22:40:47 +0530

    done

--- js/booking.js
+++ js/booking.js
@@ -3,34 +3,20 @@ const form = document.getElementById("bookingForm");
 form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
-    const formData = {
-        name: document.getElementById("fullName").value,
-        phone: document.getElementById("phoneNumber").value,
-        email: document.getElementById("email").value,
-        test: document.querySelector('select[name="Blood Test"]').value,
-        date: document.getElementById("appointmentDate").value,
-        address: document.querySelector('textarea[name="Address"]').value
-    };
+    const formData = new FormData(form);
 
-    try {
-        fetch("http://localhost:5000/book-test", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(formData)
-        });
+    const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+    });
 
-        const result = await response.json();
+    const result = await response.json();
 
-        if (result.success) {
-            alert("Booking submitted successfully!");
-            form.reset();
-        } else {
-            alert("Failed to submit booking.");
-        }
-    } catch (err) {
-    console.dir(err);
-    alert("Server error.");
-}
+    if (result.success) {
+        alert("Booking submitted successfully!");
+        form.reset();
+    } else {
+        alert("Submission failed.");
+        console.log(result);
+    }
 });
